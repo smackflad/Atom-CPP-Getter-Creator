3ce8cf8f01266679962170b63c46368fa9cf6cb5
smackflad <ask.kalamakhs@hotmail.com>
2020-02-22 22:36:33 +0200

    first commit

---
+++ spec/getter-cpp-creator-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GetterCppCreatorView from '../lib/getter-cpp-creator-view';
+
+describe('GetterCppCreatorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
